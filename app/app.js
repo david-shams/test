9a050948e03c01adfc499b13717e1f5fd240f1fa
David Shams <david.shams@gmail.com>
2016-05-05 14:50:31 -0500

    initial angular scaffolding

---
+++ app/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by david1 on 5/5/16.
+ */
+angular.module('ual', [])
+    .run(function ($rootScope) {
+        $rootScope.$on('$routeChangeSuccess', function (data) {
+            console.log('$routeChangeSuccess');
+        })
+    })
+    .controller('MainCtrl', function ($scope) {
+        $scope.company = {
+            name: 'United Airways',
+            address: '225 Wacker street',
+            city: 'Chicago'
+        }
+    });
